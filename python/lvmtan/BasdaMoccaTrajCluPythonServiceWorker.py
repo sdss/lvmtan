3a26a54bbc1154d8b1d6a02224e2888988b1cca4
Florian Briegel <briegel@mpia.de>
2022-09-22 11:06:21 +0200

    added site parameter to derot

--- python/lvmtan/BasdaMoccaTrajCluPythonServiceWorker.py
+++ python/lvmtan/BasdaMoccaTrajCluPythonServiceWorker.py
@@ -40,6 +40,11 @@ class BasdaMoccaTrajCluPythonServiceWorker(BasdaMoccaXCluPythonServiceWorker):
         self.geoloc = None
         self.sid = Siderostat()
         self.point = None
+        if (
+            self.rootNode.exist("SITE")
+            and self.rootNode.node("SITE").hasLeaf()
+        ):
+            self.site = self.rootNode.node("SITE").String.toPy()
 
     def _status(self, reachable=True):
         return {**BasdaMoccaXCluPythonServiceWorker._status(self), **{"CurrentTime": self.service.getCurrentTime() if reachable else "Unknown"}}
@@ -71,7 +76,7 @@ class BasdaMoccaTrajCluPythonServiceWorker(BasdaMoccaXCluPythonServiceWorker):
     @click.argument("RA", type=float)
     @click.argument("DEC", type=float)
     @click.argument("DELTA_TIME", type=int, default=1)
-    @click.argument("SITE", type=str, default="LCO")
+    @click.argument("SITE", type=str, default=self.site)
     @BasdaCluPythonServiceWorker.wrapper
     async def slewStart(
         self,
@@ -100,6 +105,7 @@ class BasdaMoccaTrajCluPythonServiceWorker(BasdaMoccaXCluPythonServiceWorker):
             while not self.service.moveAbsoluteCompletion().isDone():
                 await asyncio.sleep(0.1)
                 command.info(
+                    Site=self.site,
                     Position=self.service.getPosition(),
                     DeviceEncoder={"Position": self.service.getDeviceEncoderPosition("STEPS"), "Unit": "STEPS"},
                     Velocity=self.service.getVelocity(),
