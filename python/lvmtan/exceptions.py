26aa56a64e0c1aaa7747eeb3ea0422bd54981e85
Florian Briegel <briegel@mpia.de>
2022-09-22 20:31:34 +0200

    outofrange exception

--- python/lvmtan/exceptions.py
+++ python/lvmtan/exceptions.py
@@ -3,75 +3,20 @@
 #
 # Licensed under a 3-clause BSD license.
 #
-# @Author: Brian Cherinka
-# @Date:   2017-12-05 12:01:21
-# @Last modified by:   Brian Cherinka
-# @Last Modified time: 2017-12-05 12:19:32
-
-from __future__ import absolute_import, division, print_function
+# @Author: Florian Briegel
+# @Date:   2022-09-05 12:01:21
 
 
-class LvmtanError(Exception):
-    """A custom core Lvmtan exception"""
+class LvmTanError(Exception):
+    """Core exception"""
 
-    def __init__(self, message=None):
+class LvmTanNotImplemented(LvmTanError):
+    """Not yet implemented features."""
 
-        message = "There has been an error" if not message else message
+class LvmTanApiAuthError(LvmTanError):
+    """API authentication errors"""
 
-        super(LvmtanError, self).__init__(message)
-
-
-class LvmtanNotImplemented(LvmtanError):
-    """A custom exception for not yet implemented features."""
-
-    def __init__(self, message=None):
-
-        message = "This feature is not implemented yet." if not message else message
-
-        super(LvmtanNotImplemented, self).__init__(message)
+class LvmTanOutOfRange(LvmTanError):
+    """Out of range error"""
 
 
-class LvmtanAPIError(LvmtanError):
-    """A custom exception for API errors"""
-
-    def __init__(self, message=None):
-        if not message:
-            message = "Error with Http Response from Lvmtan API"
-        else:
-            message = "Http response error from Lvmtan API. {0}".format(message)
-
-        super(LvmtanAPIError, self).__init__(message)
-
-
-class LvmtanApiAuthError(LvmtanAPIError):
-    """A custom exception for API authentication errors"""
-
-    pass
-
-
-class LvmtanMissingDependency(LvmtanError):
-    """A custom exception for missing dependencies."""
-
-    pass
-
-
-class LvmtanWarning(Warning):
-    """Base warning for Lvmtan."""
-
-
-class LvmtanUserWarning(UserWarning, LvmtanWarning):
-    """The primary warning class."""
-
-    pass
-
-
-class LvmtanSkippedTestWarning(LvmtanUserWarning):
-    """A warning for when a test is skipped."""
-
-    pass
-
-
-class LvmtanDeprecationWarning(LvmtanUserWarning):
-    """A warning for deprecated features."""
-
-    pass
