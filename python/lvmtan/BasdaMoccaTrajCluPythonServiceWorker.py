f3110ab6d2bf2c7a66e3897231c33f0a2423de6a
Florian Briegel <wasndas@zone42.de>
2023-02-20 13:59:30 +0100

    added missing task init

--- python/lvmtan/BasdaMoccaTrajCluPythonServiceWorker.py
+++ python/lvmtan/BasdaMoccaTrajCluPythonServiceWorker.py
@@ -41,6 +41,8 @@ class BasdaMoccaTrajCluPythonServiceWorker(BasdaMoccaXCluPythonServiceWorker):
 
         self.schema["properties"]["SkyPA"] = {"type": "number"}
 
+        self.task = None
+
         target = None
         if (self.rootNode.exist("SITE") and self.rootNode.node("SITE").hasLeaf()):
             self.site = self.rootNode.node("SITE").String
