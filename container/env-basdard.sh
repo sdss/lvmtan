3b4d1b4d428d4199dad57e1c68da6315667b7608
Florian Briegel <wasndas@zone42.de>
2023-03-07 19:43:21 +0100

    improved tanpath in non debug

--- container/env-basdard.sh
+++ container/env-basdard.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash -l
 
+PYTHON=/usr/bin/python3
+
 BASDARD_NAME=`basename ${BASDARD_CONFIG##*/} .conf`
 test -n "$BASDARD_PORT" || BASDARD_PORT=`shuf -i 2000-65000 -n 1`
 BASDARD_ADAPTER="[NAME=$BASDARD_NAME,PORT=$BASDARD_PORT]"
@@ -9,7 +11,7 @@ if [ ${LVM_DEBUG} ]; then
     LVM_TAN_CONFIG_PATH=${LVM_ROOT}/lvm/lvmtan/python/lvmtan/config/
     PYTHONPATH=$(ls -1 -d ${LVM_ROOT}/lvm/*/python 2>/dev/null | tr "\n" ":"):$PYTHONPATH
 else
-    LVM_TAN_CONFIG_PATH=/usr/local/lib/python3.8/dist-packages/lvmtan/python/lvmtan/config/
+    LVM_TAN_CONFIG_PATH=$(${PYTHON} -c "import lvmtan as _; print(_.__path__[0])")/config
 fi
 
 INSROOT_ETC_PATH=${LVM_TAN_CONFIG_PATH}:${INSROOT_ETC_PATH}
