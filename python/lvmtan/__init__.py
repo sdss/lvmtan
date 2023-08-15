3b3af981a2861ada993c2d03cdea0e697d7046b3
José Sánchez-Gallego <gallegoj@uw.edu>
2023-08-15 13:54:30 +0000

    Tweak LVM_DEBUG

--- python/lvmtan/__init__.py
+++ python/lvmtan/__init__.py
@@ -27,8 +27,6 @@ NAME = "sdss-lvmtan"
 # than NAME.
 log = get_logger(NAME)
 
-
+print('Hiiiiiiiiiiiiii')
 # package name should be pip package name
 __version__ = get_package_version(path=__file__, package_name=NAME)
-
-
