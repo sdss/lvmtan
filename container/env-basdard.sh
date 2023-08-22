3ac83215e0c0bcbabbc64a2e754c4996980387a3
Florian Briegel <fbriegel@lvm-hub.lco.cl>
2023-08-22 10:31:50 +0000

    added tee to basdard logging /data/logs/lvmtan/basdard_2023-08-22_10:31:50.log

--- container/env-basdard.sh
+++ container/env-basdard.sh
@@ -6,6 +6,8 @@ BASDARD_NAME=`basename ${BASDARD_CONFIG##*/} .conf`
 test -n "$BASDARD_PORT" || BASDARD_PORT=`shuf -i 2000-65000 -n 1`
 BASDARD_ADAPTER="[NAME=$BASDARD_NAME,PORT=$BASDARD_PORT]"
 
+BASDARD_LOGFILE="/data/logs/lvmtan/basdard_$(date +%Y-%m-%d_%H:%M:%S).log"
+
 if [ ${LVM_DEBUG} ]; then
     LVM_TAN_CONFIG_PATH=${LVM_ROOT}/python/lvmtan/config/
     PYTHONPATH=${LVM_ROOT}/python:$PYTHONPATH
